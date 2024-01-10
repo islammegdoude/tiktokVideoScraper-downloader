37820639f201c058020d7f895ee29012c70f80ae
islam megdoude <islam.megdoude@gmail.com>
2024-01-11 00:25:35 +0100

    first commit

---
+++ scrape_video.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+import requests
+from urllib.request import urlopen
+
+# driver = webdriver.Chrome()
+# driver.get("https://www.tiktok.com/@heartdefensor")
+# #response = requests.get("https://www.tiktok.com/@heartdefensor",)
+# time.sleep(20)
+
+# soup = BeautifulSoup(driver.page_source, "html.parser")
+# videos = soup.find_all('div', class_='css-1as5cen-DivWrapper')
+
+# print()
+
+def downloadVideo(link, id):
+    print(f"Downloading video {id} from: {link}")
+    cookies = {
+        '_ga': 'GA1.1.2044991670.1704833424',
+        '_ga_ZSF3D6YSLC': 'GS1.1.1704903103.2.1.1704903320.0.0.0',
+    }
+
+    headers = {
+        'authority': 'ssstik.io',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        # 'cookie': '_ga=GA1.1.2044991670.1704833424; _ga_ZSF3D6YSLC=GS1.1.1704903103.2.1.1704903320.0.0.0',
+        'hx-current-url': 'https://ssstik.io/en',
+        'hx-request': 'true',
+        'hx-target': 'target',
+        'hx-trigger': '_gcaptcha_pt',
+        'origin': 'https://ssstik.io',
+        'referer': 'https://ssstik.io/en',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    }
+
+    params = {
+        'url': 'dl',
+    }
+
+    data = {
+        'id': link,
+        'locale': 'en',
+        'tt': 'Yzd1MVJk',
+    }
+
+    
+    print("STEP 4: Getting the download link")
+    print("If this step fails, PLEASE read the steps above")
+    response = requests.post('https://ssstik.io/abc', params=params, cookies=cookies, headers=headers, data=data)
+    downloadSoup = BeautifulSoup(response.text, "html.parser")
+
+    downloadLink = downloadSoup.a["href"]
+    videoTitle = downloadSoup.p.getText().strip()
+
+    print("STEP 5: Saving the video :)")
+    mp4File = urlopen(downloadLink)
+    # Feel free to change the download directory
+    with open(f"videos/{id}.mp4", "wb") as output:
+        while True:
+            data = mp4File.read(4096)
+            if data:
+                output.write(data)
+            else:
+                break
+
+print("STEP 1: Open Chrome browser")
+options = Options()
+options.add_argument("start-maximized")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+driver = webdriver.Chrome(options=options)
+# Change the tiktok link
+driver.get("https://www.tiktok.com/@torkx_0o")
+
+# IF YOU GET A TIKTOK CAPTCHA, CHANGE THE TIMEOUT HERE
+# to 60 seconds, just enough time for you to complete the captcha yourself.
+time.sleep(20)
+
+scroll_pause_time = 1
+screen_height = driver.execute_script("return window.screen.height;")
+i = 1
+
+print("STEP 2: Scrolling page")
+while True:
+    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+    i += 1
+    time.sleep(scroll_pause_time)
+    scroll_height = driver.execute_script("return document.body.scrollHeight;")  
+    if (screen_height) * i > scroll_height:
+        break 
+
+# this class may change, so make sure to inspect the page and find the correct class
+className = "css-1as5cen-DivWrapper"
+
+script  = "let l = [];"
+script += "document.getElementsByClassName(\""
+script += className
+script += "\").forEach(item => { l.push(item.querySelector('a').href)});"
+script += "return l;"
+
+urlsToDownload = driver.execute_script(script)
+
+print(f"STEP 3: Time to download {len(urlsToDownload)} videos")
+for index, url in enumerate(urlsToDownload):
+    print(f"Downloading video: {index}")
+    downloadVideo(url, index)
+    time.sleep(10)
